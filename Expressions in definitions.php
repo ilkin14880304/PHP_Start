4edfd603a5c16481bfb775250c4e3bf4ae9471cd
ilkin14880304 <49369916+ilkin14880304@users.noreply.github.com>
2022-05-04 09:45:05 +0400

    1.0

---
+++ Expressions in definitions.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$eurosCount = 100;
+
+// BEGIN (write your solution here)
+$dollarsCount = $eurosCount * 1.25;
+print_r($dollarsCount . "\n");
+$rublesCount = $dollarsCount * 60;
+print_r($rublesCount);
+// END
---
+++ Variables and concatenation.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$info = "We couldn't verify you mother's maiden name.";
+$intro = "\nHere is important information about your account security.";
+
+$firstName = 'Joffrey';
+$greeting = 'Hello';
+
+// BEGIN (write your solution here)
+$what = $greeting . ", " . $firstName . "!" . $intro . "\n" . $info;
+print_r($what);
+// END
