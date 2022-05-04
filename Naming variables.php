8fba0c3946598ba127650f27b920b0368982854d
ilkin14880304 <49369916+ilkin14880304@users.noreply.github.com>
2022-05-04 10:01:39 +0400

    1.0

---
+++ Naming variables.php
@@ -0,0 +1,8 @@
+<?php
+
+
+// BEGIN (write your solution here)
+$firstNumber = 1.10;
+$secondNumber = -100;
+printf($firstNumber * $secondNumber);
+// END
---
+++ magic numbers.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$king = 'King Balon the 6th';
+
+// BEGIN (write your solution here)
+$castle = 6;
+$rooms = 17;
+
+print_r($king . ' has ' . ($castle * $rooms) . ' rooms.');
+// END
