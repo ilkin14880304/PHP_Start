734dd4349c2f3f955b32382e52449a0240ddc9ba
ilkin14880304 <49369916+ilkin14880304@users.noreply.github.com>
2022-05-05 07:31:17 +0400

    1.0

---
+++ Function call — expression.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$text = 'Never forget what you are, for surely the world will not';
+
+// BEGIN (write your solution here)
+print_r("First: {$text[0]}\nLast: {$text[strlen($text) -1]}");
+// END
